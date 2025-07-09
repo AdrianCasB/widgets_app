b3ca22e8caa0bd3d315aa7eb5cea0d3bc9158abd
Adrián Castillo <adriancb99@hotmail.com>
2025-07-09 14:24:58 +0200

    implement home screen list view

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Flutter Widgets')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Widgets')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({required this.menuItem});
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      title: Text(menuItem.title),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {
+        //TODO: navegar en otra pantalla
+      },
+    );
   }
 }
