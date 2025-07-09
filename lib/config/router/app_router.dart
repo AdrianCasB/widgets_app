2e4e29b199d3e40e5851387cdad3a3d1e8d027eb
Adrián Castillo <adriancb99@hotmail.com>
2025-07-09 16:33:44 +0200

    feat: config go_router

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:go_router/go_router.dart';
+import '../../presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
+    GoRoute(path: '/buttons', builder: (context, state) => ButtonsScreen()),
+    GoRoute(path: '/cards', builder: (context, state) => CardsScreen()),
+  ],
+);
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -27,9 +29,9 @@ class _HomeView extends StatelessWidget {
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({required this.menuItem});
-
   final MenuItem menuItem;
+
+  const _CustomListTile({required this.menuItem});
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +43,11 @@ class _CustomListTile extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        //TODO: navegar en otra pantalla
+        context.push(menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        // Navigator.of(
+        //   context,
+        // ).push(MaterialPageRoute(builder: (context) => const ButtonsScreen()));
       },
     );
   }
