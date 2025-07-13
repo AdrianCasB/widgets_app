ed81f1cec1216089cc763ec86cbb5a82ebd60d79
Adrián Castillo <adriancb99@hotmail.com>
2025-07-13 18:23:29 +0200

    feat: progress indicator finished

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -18,5 +18,10 @@ final appRouter = GoRouter(
       path: '/cards',
       builder: (context, state) => CardsScreen(),
     ),
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => ProgressScreen(),
+    ),
   ],
 );
