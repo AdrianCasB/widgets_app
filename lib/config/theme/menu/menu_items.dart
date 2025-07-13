22808650c593ceeae42cc183053f379563f4f341
Adrián Castillo <adriancb99@hotmail.com>
2025-07-14 01:02:37 +0200

    feat: ui screens finished

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -42,4 +42,18 @@ const appMenuItems = <MenuItem>[
     link: '/snackbars',
     icon: Icons.info_outline,
   ),
+
+  MenuItem(
+    title: 'Animated container',
+    subtitle: 'Stateful widget animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
+
+  MenuItem(
+    title: 'UI Controls + Tiles',
+    subtitle: 'Una serie de controles de Flutter',
+    link: '/ui-controls',
+    icon: Icons.car_rental_outlined,
+  ),
 ];
